20a26b6250f68ccd6446d3b7ace6eca4a53d4c7f
Isager <p.isager@tue.nl>
2020-05-20 11:08:07 +0200

    moved data related to IRR analyses into separate folder in raw_data and created analysis file for these analyses

---
+++ analysis/dataset_B_inter_rater_reliability.R
@@ -0,0 +1,17 @@
+
+# Load relevant datasets
+
+df.orig <- 
+
+# Create output dataset
+
+## Combine sample size columns into one dataset
+
+## Create dummy column "n_ir_identical" [0, 1] to denote when all raters agree and not.
+
+## Create "n_final" column that will contain the final sample size after resolving disagreements. Populate this variable with cases where all coders agreed.
+
+
+
+# Calculate inter-rater reliability
+
