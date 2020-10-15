491124ae1e3fa4aa0e34f489e42a817563fe091d
Peder Isager <pederisager@gmail.com>
2020-10-15 09:17:46 +0200

    added funtion to add/update altmetric scores to wos data

---
+++ analysis/wos_data_update_Altmetric_score.R
@@ -0,0 +1,29 @@
+
+#### Add Scopus citation counts to WoS bibliometrics ####
+
+# Load packages
+
+library(rAltmetric)
+
+# Load and prepare data
+
+data <- readRDS("../processed_data/metadata_WoS/wos_all_records_data.Rds")
+data$DI[data$DI==""] <- NA
+
+# Add Scopus citations
+
+for (i in 1:nrow(data)) {
+  
+  print(data$DI[i])
+  
+  altmetric_score <- try(altmetrics(doi = data$DI[i])[["score"]])  # Extract bibliometric JSON and fetch citation count from the object
+  
+  data$altmetric_score[i] <- ifelse(test = is.character(altmetric_score), 
+                                    yes = NA, 
+                                    no = altmetric_score)  # if null is returned, Scopus failed to find a reference for the DOI, and NA is returned to data$scopus_citations
+  
+}
+
+# Save data
+saveRDS(file = "wos_all_records_data.Rds", object = data)
+write.table(x = data, file = "wos_all_records_data.tsv", sep = "\t", row.names = F)
