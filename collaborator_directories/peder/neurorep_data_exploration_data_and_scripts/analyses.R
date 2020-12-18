1185581ce412b0e52a9ce80ac3966b69615a7329
Peder Isager <pederisager@gmail.com>
2020-12-18 16:01:08 +0100

    removed collaborator_directories from gitignore and adding these directories to github

---
+++ collaborator_directories/peder/neurorep_data_exploration_data_and_scripts/analyses.R
@@ -0,0 +1,454 @@
+###############################
+# Exploratory analysis script #
+###############################
+
+
+#### Load packages #### 
+
+library(tidyverse)
+library(psych)
+library(gridExtra)
+library(corrplot)
+
+
+
+#### Load and clean data #### 
+
+source("load_and_clean_data.R")
+
+
+
+#### Analyses #### 
+
+# Using the codebook 
+
+## To check the description of a variable, use $ to find the variable in codebook.data.all. E.g.:
+
+codebook$UT
+codebook$coder
+codebook$tcs
+
+## To view the entire codebook: 
+
+view(t(codebook))
+
+## The codebook contains a description of all variables contained in data.all and data.bib. 
+
+
+
+# Dataset summaries
+
+## data.bib
+ 
+pubyear.freq <- table(data.bib$PY)
+barplot(pubyear.freq, xlab = "Publication year")  # Plot frequency of publication years
+
+hist(data.bib$TC_2020, breaks = 50, xlab = "Citation count (WoS)", main = "")  # Plot citation count distribution
+
+corrplot(cor(select(data.bib, 
+           TC, 
+           TC_2020, 
+           crossref_citations, 
+           scopus_citations, 
+           tcs, 
+           altmetric_score), 
+    use = "complete.obs") ) # Print correlation matrix for various citation count sources - all are highly intercorrelated, except altmetric count, which is weakly correlated with all other sources (as is known from literature). 
+
+
+
+## data.all
+
+
+describe(select(data.all, 
+                TC_2020,
+                years.since.pub, 
+                sample_size,
+                RV))  # Descriptive statistics for RV and all variables used to calculate it
+
+par(mfrow=c(2,2))
+
+hist(data.all$sample_size, breaks = 300, xlab = "Sample size", main = "")  #Plot sample size (cut values over 200)
+hist(data.all$TC_2020, breaks = 50, xlab = "Citation counts", main = "")  #Plot citation count
+barplot(table(data.all$PY), xlab = "Publication year", ylab = "Frequency")  # Plot log sample size
+hist(data.all$RV, breaks = 50, xlab = "Replicaton value", main = "")  #Plot replication value
+
+par(mfrow=c(1,1))  # Reset par(mfrow)
+
+### Test if sample size is uncorrelated with yearly citation count, as is predicted by the psychometric model in thesis chapter 2
+
+cor.test(data.all$sample_size, data.all$TC_2020/data.all$years.since.pub, na.action = "na.rm")  # correlation test - likely incorrectly specified given the massive skew of both variables
+
+par(mfrow=c(2,1))
+
+plot(data.all$sample_size, data.all$TC_2020/data.all$years.since.pub, xlab="sample size", ylab="citation count per year")  # plot sample size by citation count, raw
+plot(log(data.all$sample_size), log(data.all$TC_2020/data.all$years.since.pub), xlab="log sample size", ylab="log citation count per year")  # plot sample size by citation count, log
+
+par(mfrow=c(1,1))  # Reset par(mfrow)
+
+## data.irr
+
+ICC(data.irr[, c("sample_size_orig", "sample_size_BA", "sample_size_PhD")])  # Intraclass correlation between the three sample size coders
+
+corrplot(cor(select(.data = data.irr, 
+           sample_size_orig, 
+           sample_size_BA, 
+           sample_size_PhD, 
+           sample_size_final)))  # Correlation matrix between the three different coders and the final sample size after resolving disagreements
+
+
+
+
+
+
+
+
+# Part 1 - describe the sample of candidates in terms of their bibliometrics 
+#
+# RQ: Is our sample representative of the population "fmri research in social psychology"?
+
+## 1. Plot VOS viewer word cloud (done outside of R)
+
+write.csv(x = data.bib, file = "../../../processed_data/wos+cwts_nonexcluded_records_data.csv", row.names = F)  # Save dataset to be used by VOSviewer
+
+## 2. Describe number of CWTS clusters, distribution of cluster size, and most frequent keyterms used to describe clusters
+
+nrow(unique(select(data.bib, cluster_id1, n_pubs)))  # Number of unique clusters/subfields contained in set
+
+### Plot most frequent primary cluster labels
+
+labels <- c(data.bib$label1, 
+            data.bib$label2, 
+            data.bib$label3, 
+            data.bib$label4, 
+            data.bib$label5)  # Combine all labels into one string variable
+
+lab.freq <- as.data.frame(tail(sort(table(labels)), 50))  # count prevalence of each string and save 50 most prevalent
+
+g.lab.freq <- ggplot(lab.freq, aes(x = labels, y = Freq)) +
+  geom_col() +
+  labs(title="Most frequent primary cluster labels",
+       x = NULL,
+       y = "Frequency") +
+  coord_flip() +
+  theme_bw()
+
+### Plot distribution of cluster size
+
+clu.siz <- unique(select(data.bib, cluster_id1, n_pubs))
+
+g.clu.siz <- ggplot(clu.siz, aes(x = n_pubs)) +
+  geom_histogram() +
+  labs(title="Number of publications in clusters/subfields",
+       x = "Cluster size",
+       y = "Frequency") +
+  theme_bw()
+
+
+### Plot journals most frequently published in
+
+jou.freq <- as.data.frame(tail(sort(table(data.bib$SO)), 20))
+levels(jou.freq$Var1) <- sub("PROCEEDINGS OF THE NATIONAL ACADEMY OF SCIENCES OF THE UNITED STATES OF AMERICA", "PNAS", levels(jou.freq$Var1))
+
+
+g.jou.freq <- ggplot(jou.freq, aes(x = Var1, y = Freq)) +
+  geom_col() +
+  labs(title="Journals most frequently published in",
+       x = NULL,
+       y = "Frequency") +
+  coord_flip() +
+  theme_bw()
+
+
+### Plot WoS research fields most frequently tagged
+
+field.freq <- as.data.frame(tail(sort(table(data.bib$WC)), 20))
+
+
+g.field.freq <- ggplot(field.freq, aes(x = Var1, y = Freq)) +
+  geom_col() +
+  labs(title="WoS research fields most frequently tagged",
+       x = NULL,
+       y = "Frequency") +
+  coord_flip() +
+  theme_bw()
+
+
+### Plot frequent cluster labels and cluster size distribution side by side
+grid.arrange(g.lab.freq, g.clu.siz, ncol=2)
+
+
+
+#### Analyses: Variation in RV across variation in input and calculation ####
+
+# Separate out the top 100 RV studies in the dataset for separate analyses 
+
+data.all.top100 <- data.all %>% arrange(., -RV, ) %>% head(100)
+
+
+
+# Analyze "value" indicator distributions and their cross-correlations
+
+## data.bib
+
+### Distributions
+
+ggplot(data = data.bib) +
+  geom_density(aes(x = TC_2020), col = "black") +
+  geom_density(aes(x = crossref_citations), col = "red") + 
+  geom_density(aes(x = scopus_citations), col = "blue") +
+  geom_density(aes(x = tcs), col = "green") +
+  geom_density(aes(x = TC), col = "purple") +
+  theme_bw()
+
+ggplot(data = data.bib, aes(x = tncs)) +
+  geom_density() 
+
+ggplot(data = data.bib, aes(x = altmetric_score)) +
+  geom_density() 
+
+### Correlation matrix
+
+cor(select(.data = data.bib, 
+           TC_2020, 
+           TC, 
+           crossref_citations, 
+           scopus_citations,
+           tcs,
+           tncs,
+           altmetric_score), use = "pairwise.complete.obs", method = "spearman")
+
+## data.all
+
+### Distributions
+
+ggplot(data = data.all) +
+  geom_density(aes(x = TC_2020), col = "black") +
+  geom_density(aes(x = crossref_citations), col = "red") + 
+  geom_density(aes(x = scopus_citations), col = "blue") +
+  geom_density(aes(x = tcs), col = "green") +
+  geom_density(aes(x = TC), col = "purple") +
+  theme_bw()
+
+ggplot(data = data.all, aes(x = tncs)) +
+  geom_density() 
+
+ggplot(data = data.all, aes(x = altmetric_score)) +
+  geom_density() 
+
+### Correlation matrix
+
+cor.dat <- select(.data = data.all, 
+       TC_2020, 
+       TC, 
+       crossref_citations, 
+       scopus_citations,
+       tcs,
+       tncs,
+       altmetric_score, 
+       sample_size, 
+       years.since.pub)
+
+cor.dat$TC_2020_by_year <- cor.dat$TC_2020/cor.dat$years.since.pub
+
+cor.mat <- cor(cor.dat, use = "pairwise.complete.obs", method = "spearman")
+
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))  # Set color scheme for corrplot
+
+corrplot(corr = cor.mat, 
+         method = "color", 
+         col = col(200), 
+         addCoef.col = "black", 
+         tl.col="black", 
+         tl.srt=45, 
+         order="alphabet", 
+         diag = F)
+
+
+
+
+## Top 100 data
+
+cor.dat <- select(.data = data.all.top100, 
+                  TC_2020, 
+                  TC, 
+                  crossref_citations, 
+                  scopus_citations,
+                  tcs,
+                  tncs,
+                  altmetric_score, 
+                  sample_size, 
+                  years.since.pub)
+
+cor.dat$TC_2020_by_year <- cor.dat$TC_2020/cor.dat$years.since.pub
+
+cor.mat <- cor(cor.dat, use = "pairwise.complete.obs", method = "spearman")
+
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))  # Set color scheme for corrplot
+
+corrplot(corr = cor.mat, 
+         method = "color", 
+         col = col(200), 
+         addCoef.col = "black", 
+         tl.col="black", 
+         tl.srt=45, 
+         order="alphabet", 
+         diag = F)
+
+
+
+
+# Analyze sample size distribution and inter-rater reliability
+
+## All data
+
+### Distribution
+
+describe(data.all$sample_size)
+
+ggplot(data = data.all, aes(x = sample_size)) + 
+  geom_density() +
+  theme_bw()
+
+### Inter-rater reliability
+
+#### Intra-class correlation coefficent
+ICC(select(data.irr, sample_size_orig, sample_size_BA, sample_size_PhD))
+
+#### Correlation matrix
+irr.mat <- cor(select(data.irr, sample_size_orig, sample_size_BA, sample_size_PhD, sample_size_final))
+
+corrplot(corr = irr.mat, 
+         method = "color", 
+         col = col(200), 
+         addCoef.col = "black", 
+         tl.col="black", 
+         tl.srt=45, 
+         order="hclust", 
+         diag = F)
+
+#### Coder-by-coder scatter plots
+p.orig_BA <- ggplot(data = data.irr, aes(x = sample_size_orig, y = sample_size_BA)) +
+  geom_point() +
+  theme_bw()
+
+p.orig_PhD <- ggplot(data = data.irr, aes(x = sample_size_orig, y = sample_size_PhD)) +
+  geom_point() +
+  theme_bw()
+
+p.BA_PhD <- ggplot(data = data.irr, aes(x = sample_size_BA, y = sample_size_PhD)) +
+  geom_point() +
+  theme_bw()
+
+grid.arrange(p.orig_BA, p.orig_PhD, p.BA_PhD)
+
+## Top 100 data
+
+
+
+# Publication year, distribution and analyses
+
+## All data
+
+### Distribution of publication year/age
+
+plot(TC_2020~PY, data = data.all)
+abline(lm(TC_2020 ~ PY, data = data.all), col = "blue")
+
+plot(log(sample_size)~PY, data = data.all)
+abline(lm(log(sample_size) ~ PY, data = data.all), col = "blue")
+
+plot(log(TC_2020)~log(sample_size), data = data.all)
+
+
+### Correlation between age and citation count
+
+plot(TC_2020^(1/3) ~ PY, data = data.all)
+abline(lm(TC_2020^(1/3) ~ PY, data = data.all), col = "blue")
+
+### Correlation between age and (citation count) / age
+
+plot(TC_2020^(1/3) ~ PY, data = data.all)
+abline(lm(TC_2020^(1/3) ~ PY, data = data.all), col = "blue")
+
+### Correlation between age and sample size
+
+plot(sample_size^(1/3) ~ PY, data = data.all)
+abline(lm(sample_size^(1/3) ~ PY, data = data.all), col = "blue")
+
+### Correlation between citation count and sample size, controlling for age
+
+cor.test(data.all$TC_2020, data.all$sample_size, method = "spearman")
+ppcor::pcor.test(data.all$TC_2020, data.all$sample_size, data.all$years.since.pub, method = "spearman")
+
+x <- data.all
+x$TC2020_plus1 <- x$TC_2020+1 
+
+model.1 <- glm(formula = TC2020_plus1 ~ sample_size, 
+               data = x, 
+               family = Gamma(link=log))
+
+coefficients(model.1)
+
+summary(model.1)
+
+
+model.2 <- glm(formula = TC2020_plus1 ~ sample_size + years.since.pub, 
+              data = x, 
+              family = Gamma(link=log))
+
+coefficients(model.2)
+
+summary(model.2)
+
+reghelper::beta(model.1)
+reghelper::beta(model.2)
+
+
+
+
+
+
+# Analyze RV distribution and cross correlation
+
+## data.all
+
+RV.dat <- select(data.all, TC_2020, TC, crossref_citations, scopus_citations, tcs, tncs, altmetric_score)
+RV.dat$TC_2020_plus_altmetric <- RV.dat$TC_2020 + RV.dat$altmetric_score
+
+RV.dat <- (RV.dat/data.all$years.since.pub+1) * (1/data.all$sample_size)  # Calculate RV for all value indicators 
+
+RV.mat <- cor(RV.dat, use = "pairwise.complete.obs", method = "spearman")
+
+corrplot(corr = RV.mat, 
+         method = "color", 
+         col = col(200), 
+         addCoef.col = "black", 
+         tl.col="black", 
+         tl.srt=45, 
+         order="hclust", 
+         diag = F)
+
+
+## data.all.top100
+
+RV.dat <- select(data.all.top100, TC_2020, TC, crossref_citations, scopus_citations, tcs, tncs, altmetric_score)
+RV.dat$TC_2020_plus_altmetric <- RV.dat$TC_2020 + RV.dat$altmetric_score
+
+RV.dat <- (RV.dat/data.all$years.since.pub+1) * (1/data.all$sample_size)  # Calculate RV for all value indicators 
+
+RV.mat <- cor(RV.dat, use = "pairwise.complete.obs", method = "spearman")
+
+corrplot(corr = RV.mat, 
+         method = "color", 
+         col = col(200), 
+         addCoef.col = "black", 
+         tl.col="black", 
+         tl.srt=45, 
+         order="hclust", 
+         diag = F)
+
+
+
+
+
+
---
+++ collaborator_directories/peder/neurorep_data_exploration_data_and_scripts/load_and_clean_data.R
@@ -0,0 +1,93 @@
+#### Load packages ####
+
+library(tidyverse)
+
+#### Load data #### 
+
+data.bib <- readRDS("wos+cwts_all_records_data.Rds")  # Load dataset containing Web of Science and CWTS bibliometrics for all records identified by our initial literature search.
+data.all <- readRDS("dataset_A_updated_data.rds")  # Load dataset randomly sampled from data.bib for which sample size has also been coded 
+data.irr <- readRDS("dataset_B_inter_rater_reliability.Rds")  # Load dataset randomly sampled from data.all, used for checking sample size coding inter-rater reliability
+codebook <- read.table("codebook.tsv", header = T, row.names = 1, sep = "\t")
+
+
+
+#### Clean data ####
+
+# Exclude cases from data.bib deemed irrelevant for our study selection based on keywords
+
+tit.abs.keyw <- paste(data.bib$TI, data.bib$AB, data.bib$ID, data.bib$DE) # Combine title, abstract and keyword information into one column for grep to search
+
+include <- c("\\bfmri\\b")  # include only records containing the following terms. "\\b" wrappers makes sure only whole words are matched.
+
+exclude <- c("\\badhd\\b",
+             "\\banimal\\b",
+             "\\bmonkey\\b",
+             "\\bochrogaster\\b",
+             "\\brat\\b",
+             "\\bmouse\\b",
+             "\\bautism\\b", 
+             "\\beeg\\b", 
+             "\\bdepression\\b", 
+             "\\bdisorder\\b", 
+             "\\bdisorders\\b", 
+             "\\bschizophrenia\\b")  # Define list of terms to exclude. "\\b" wrappers makes sure only whole words are matched.
+
+exclude.list <- read.csv("wos_excluded_keywords.csv", stringsAsFactors = FALSE)  # Load list of to-be-excluded terms, as decided on by the authors
+
+exclude.words <- exclude.list$Term[exclude.list$exclude...final=="yes"]
+exclude.words <- paste0("\\b", exclude.words, "\\b")  # Define list of terms to exclude. "\\b" wrappers makes sure only whole words are matched
+include.l <- grepl(pattern = paste(include, collapse = "|"), x = tit.abs.keyw, ignore.case = TRUE)  # identify indexes in data matching term(s)
+exclude.l <- grepl(pattern = paste(exclude.words, collapse = "|"), x = tit.abs.keyw, ignore.case = TRUE)  # identify indexes in data matching term(s)
+data.bib <- data.bib[include.l & !exclude.l,]  # filter rows based on identified terms
+data.bib <- data.bib[!duplicated(data.bib$DI),]  # Exclude duplicate records, identified through duplicate DOIs. The higher row index duplicate will be excluded.
+
+rm(include.l, exclude.l, exclude.list, exclude.words, include, exclude, tit.abs.keyw)
+
+# transpose codebook to facilitate easy look-up of variable definitions
+
+codebook <- as.data.frame(t(codebook))
+
+# Set numeric variables to numeric in data.bib and data.all
+
+data.bib$TC <- as.numeric(data.bib$TC)
+data.bib$TC_2020 <- as.numeric(data.bib$TC_2020)
+data.bib$PY <- as.numeric(data.bib$PY)
+
+data.all$TC <- as.numeric(data.all$TC)
+data.all$TC_2020 <- as.numeric(data.all$TC_2020)
+data.all$PY <- as.numeric(data.all$PY)
+
+# Remove excluded rows from data.all
+
+data.all <- data.all[is.na(data.all$excluded),]
+
+# Add missing values in TC_2020 and PY to data.all and recalculate RV
+
+missing.data.all <- read.csv("dataset_A_missing_TC_2020_and_PY.csv")  # missing PY and TC_2020 values, manually identified in WoS
+
+for (i in 1:nrow(data.all)) {
+  if (is.na(data.all$PY[i]))
+    data.all$PY[i] <- missing.data.all$PY[which(missing.data.all$UT==data.all$UT[i])[1]]
+  
+  if (is.na(data.all$TC_2020[i]))
+    data.all$TC_2020[i] <- missing.data.all$TC_2020[which(missing.data.all$UT==data.all$UT[i])[1]]
+  
+}
+
+rm(missing.data.all)
+
+# Calculate RV for data.all
+
+data.all$years.since.pub <- 2020 - data.all$PY
+data.all$RV <- (data.all$TC_2020 / (data.all$years.since.pub + 1)) * (1 / data.all$sample_size)  # Calculate RV for all records in data.all
+
+# Trim data.irr down to relevant variables
+
+data.irr <- select(data.irr, 
+                   UT, DI, AU, TI, PY, 
+                   coder, sample_size_orig, sample_size_BA, sample_size_PhD, sample_size_final,
+                   matches_orig_BA, matches_orig_PhD, matches_BA_PhD, matches_all, irr_resolver, irr_resolver_comment)
+
+saveRDS(data.bib, file = "data_bib.Rds")
+saveRDS(data.all, file = "data_all.Rds")
+saveRDS(data.irr, file = "data_irr.Rds")
